8937c3d6f8c991d999fb8b0325aed35af53c24fb
aframirezh <aframirezh@gmail.com>
2026-01-05 13:21:14 -0500

    chore: setup vite react-ts and tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-grey-500">
+      <header className="border-b bg-white">
+        <div className="mx-auto max-w-6xl px-4 py-4">
+          <h1 className="text-xl font-semibold">Proxet Test - Paginated DataList</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
